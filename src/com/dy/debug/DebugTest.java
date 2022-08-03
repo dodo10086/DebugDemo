e7f3410fa0979c075c0fa161c1673f7f77b48088
dy <17301588251@163.com>
2022-08-03 14:57:07 +0800

    Initial commit

---
+++ src/com/dy/debug/DebugTest.java
@@ -0,0 +1,34 @@
+package com.dy.debug;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author dy
+ * @date 2022/8/3 13:26
+ */
+public class DebugTest {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+        HashMap<String,String> map = new HashMap<>();
+        map.put("name","dy");
+        map.put("age","22");
+        map.put("school","Tsinghua");
+        map.put("major","computer");
+
+        String age = map.get("age");
+        System.out.println(age);
+        System.out.println("age = " + age);
+
+        map.remove("major");
+        System.out.println(map);
+
+
+    }
+
+
+
+}
